ce080d5bc36219605b8a9e9a8be41706d6943f58
jiaxinwang <jiaxinwang@me.com>
2020-08-21 12:38:54 +0800

    + response

--- middleware.go
+++ middleware.go
@@ -83,3 +83,30 @@ func Middleware(c *gin.Context) {
 	}()
 	c.Next()
 }
+
+// MiddlewareResponse ...
+func MiddlewareResponse(c *gin.Context) {
+	defer func() {
+		ret := make(map[string]interface{})
+		if v, ok := c.Get("ret"); ok {
+			ret = v.(map[string]interface{})
+		}
+		if _, ok := ret["data"]; !ok {
+			ret["data"] = nil
+		}
+		if _, ok := ret["error_no"]; !ok {
+			if _, ok := ret["error_msg"]; ok {
+				ret["error_no"] = 400
+			} else {
+				ret["error_no"] = 0
+			}
+		}
+		if _, ok := ret["error_msg"]; !ok {
+			ret["error_msg"] = ``
+		}
+
+		ret["request_id"] = c.MustGet("requestID")
+		c.JSON(200, ret)
+	}()
+	c.Next()
+}
