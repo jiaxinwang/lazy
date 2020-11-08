ceb5a933e3ff4591c2cbed1a493d287ed3b6e3ae
jiaxinwang <jiaxinwang@me.com>
2020-11-08 22:49:18 +0800

    + split params

--- values.go
+++ values.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/mitchellh/mapstructure"
@@ -192,83 +191,6 @@ func dbNameWithFieldName(v interface{}, fieldName string) (string, error) {
 
 }
 
-// TagSlice ...
-func TagSlice(v interface{}, params map[string][]string) map[string][]interface{} {
-	ret := make(map[string][]interface{})
-	val := reflect.ValueOf(v).Elem()
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Type().Field(i)
-		dbName, err := dbNameWithFieldName(v, field.Name)
-		if err != nil {
-			logrus.WithError(fmt.Errorf("can't find schema field name: %s", field.Name)).Warn()
-			continue
-		}
-		if t := dbName; t != `` {
-			if vv, ok := params[t]; ok {
-				ret[t] = make([]interface{}, 0)
-				for _, vvv := range vv {
-					ret[t] = append(ret[t], Parse(vvv, field.Type.Kind()))
-				}
-			}
-		}
-	}
-
-	return ret
-}
-
-// Tag ...
-func Tag(v interface{}, m map[string]string) map[string]interface{} {
-	ret := make(map[string]interface{})
-	val := reflect.ValueOf(v).Elem()
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Type().Field(i)
-		name, err := dbNameWithFieldName(v, field.Name)
-		if err != nil {
-			logrus.WithError(fmt.Errorf("can't find schema field name: %s", field.Name)).Warn()
-			continue
-		}
-
-		if t := name; t != `` {
-			if vv, ok := m[t]; ok {
-				name := field.Name
-				r := reflect.ValueOf(v)
-				f := reflect.Indirect(r).FieldByName(name)
-				fieldValue := f.Interface()
-				switch vvv := fieldValue.(type) {
-				case uint64:
-					i, _ := strconv.ParseUint(vv, 10, 64)
-					ret[t] = i
-				case uint32:
-					i, _ := strconv.ParseUint(vv, 10, 32)
-					ret[t] = i
-				case uint:
-					i, _ := strconv.ParseUint(vv, 10, 64)
-					ret[t] = int(i)
-				case int64:
-					i, _ := strconv.ParseInt(vv, 10, 64)
-					ret[t] = i
-				case int32:
-					i, _ := strconv.ParseInt(vv, 10, 32)
-					ret[t] = i
-				case int:
-					i, _ := strconv.ParseInt(vv, 10, 64)
-					ret[t] = int(i)
-				case string:
-					ret[t] = vv
-				case bool:
-					ret[t], _ = strconv.ParseBool(vv)
-				case time.Time:
-					ret[t], _ = time.Parse(time.RFC3339, vv)
-				default:
-					_ = vvv
-				}
-			}
-		}
-	}
-	return ret
-}
-
 // QueryParam ...
 type QueryParam struct {
 	Model     interface{}
@@ -300,7 +222,7 @@ func toGenericArray(arr ...interface{}) []interface{} {
 	return arr
 }
 
-func splitParams1(model interface{}, params map[string][]string) (queryParam QueryParam, err error) {
+func splitQueryParams(model interface{}, params map[string][]string) (queryParam QueryParam, err error) {
 	queryParam.Eq = make(map[string][]interface{})
 	queryParam.Gt = make(map[string][]interface{})
 	queryParam.Lt = make(map[string][]interface{})
@@ -437,74 +359,5 @@ func splitParams1(model interface{}, params map[string][]string) (queryParam Que
 		}
 	}
 
-	return
-}
-
-// splitParams ...
-func splitParams(params map[string][]string) (eq map[string][]string, gt, lt, gte, lte map[string]string) {
-	eq = make(map[string][]string)
-	gt = make(map[string]string)
-	lt = make(map[string]string)
-	gte = make(map[string]string)
-	lte = make(map[string]string)
-
-	for k, vv := range params {
-		if vv == nil {
-			continue
-		}
-		for _, v := range vv {
-			name := k
-			destM := &eq
-			destS := &gt
-			switch {
-			case strings.EqualFold(k, "limit"):
-				fallthrough
-			case strings.EqualFold(k, "offset"):
-				fallthrough
-			case strings.EqualFold(k, "page"):
-				destM = nil
-				destS = nil
-			case strings.HasSuffix(k, `_gt`):
-				name = strings.TrimSuffix(k, `_gt`)
-				destM = nil
-				destS = &gt
-			case strings.HasSuffix(k, `_lt`):
-				name = strings.TrimSuffix(k, `_lt`)
-				destM = nil
-				destS = &lt
-			case strings.HasSuffix(k, `_gte`):
-				name = strings.TrimSuffix(k, `_gte`)
-				destM = nil
-				destS = &gte
-			case strings.HasSuffix(k, `_lte`):
-				name = strings.TrimSuffix(k, `_lte`)
-				destM = nil
-				destS = &lte
-			default:
-				destM = &eq
-				destS = nil
-			}
-			if destS != nil {
-				(*destS)[name] = v
-			}
-			if destM != nil {
-				if (*destM)[name] == nil {
-					(*destM)[name] = make([]string, 0)
-				}
-				(*destM)[name] = append((*destM)[name], v)
-			}
-		}
-	}
-	return
-}
-
-// URLValues ...
-func URLValues(s interface{}, q map[string][]string) (eqm map[string][]interface{}, gtm, ltm, gtem, ltem map[string]interface{}) {
-	eq, gt, lt, gte, lte := splitParams(q)
-	eqm = TagSlice(s, eq)
-	gtm = Tag(s, gt)
-	ltm = Tag(s, lt)
-	gtem = Tag(s, gte)
-	ltem = Tag(s, lte)
 	return
 }
